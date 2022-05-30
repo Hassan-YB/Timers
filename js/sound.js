cb74c38ebb5228314ad0a1cb5f793c2153c23e93
Hassan-YB <hassanbaras5@gmail.com>
2022-05-31 01:45:32 +0500

    Countdown timer updated

---
+++ js/sound.js
@@ -0,0 +1,14 @@
+function playStartSound(){
+    var audio = new Audio('../sound/a6_sine_250ms.ogg');
+    audio.play();
+}
+
+function playStopSound(){
+    var audio = new Audio('../sound/a6_sine_100ms_end.ogg');
+    audio.play();
+}
+
+function playIntervalSound(){
+    var audio = new Audio('../sound/a6_sine_750ms.ogg');
+    audio.play();
+}
